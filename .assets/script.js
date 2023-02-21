7b002a7eb044e40b48c260e4f8a8916815ac1c8d
ssimmons122 <121777930+ssimmons122@users.noreply.github.com>
2023-02-20 21:53:08 -0500

    var to js

--- .assets/script.js
+++ .assets/script.js
@@ -15,59 +15,76 @@ function startTimer() {
         }
     }, 1000);
 }
-    
+
+var score = 0;
+var questionIndex = 0;
 
 //timer variables 
-    var currentTime = document.querySelector("#currentTime");
     var timer = document.querySelector("#startTime");
+    var liveTimer = document.querySelector("#liveTime");
     var questionsDiv = document.querySelector("#questionsDiv");
+    //start timer, 15 sec/question
+    var secondsLeft = 75; 
+    //when wrong answer
+    var handicap = 10;
+    var ulCreate = document.createElement("ul");
    
 //question variables 
-    let q = {
-        inquiry: "Commonly used data types DO NOT include:",
-        options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
-        correctOption: 3,
-    };
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        answer: "3. alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+        answer: "3. parentheses"
+    },
+    {
+        title: "Arrays in Javascript can be used to store ____.",
+        choices: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        answer: "4. all of the above"
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["1. commas", "2. curly brackets", "3. quotes", "3. parenthesis"],
+        answer: "1. quotes"
+    },
+    {
+        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["1. Javascript", "2. terminal/bash", "3. for loops", "4. console log"],
+        answer: "4. console log"
+    },
 
-//define object
-let question = {
-    inquiry: "Commonly used data types DO NOT include:",
-    options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
-    correctOption: 3,
-};
+];
 
 
 //call function for question
-function askQuestion (q) {
-        let inquiryDiv = document.getElementById('inquiry');
-        inquiryDiv.textContent = q.inquiry;
-
-
 let option = document.querySelectorAll('.options');
   console.log(option);
   option.forEach(function(options, index){
     element.textContent = q.options[index];
     
-    //element.addEventListener('click', function(){
-        
-      });
-    };
-  
-  //call the function
-function askQuestion(question)
-    askQuestion(question);
-
-
-  //add event listener to button
-  let button = document.getElementById('button');
-button.addEventListener('click', function () {
-        console.log('Clicked!');
-     if (q.correctOption == index) {
-        console.log('Correct!');
-       } else {
-            console.log('Incorrect!');
-          }
     });
+   
+  
+ //add event listener to button
+ timer.addEventListener("click", function () {
+    if (holdInterval === 0) {
+        holdInterval = setInterval(function () {
+            secondsLeft--;
+            currentTime.textContent = "Time: " + secondsLeft;
+
+            if (secondsLeft <= 0) {
+                clearInterval(holdInterval);
+                allDone();
+                currentTime.textContent = "Time's up!";
+            }
+        }, 1000);
+    }
+    render(questionIndex);
+});
  
 
 
